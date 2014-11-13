968056499af5f781c3e8271ddaa54c2840a69f31
PharaohKJ <kato@phalanxware.com>
2014-11-14 04:06:20 +0900

    write about new parameter.

--- app.rb
+++ app.rb
@@ -11,13 +11,16 @@ IDOBATA_HOST = 'idobata.io'
 
 class App < Sinatra::Base
   post '/push.json' do
+
     data = JSON.parse request.body.read
+
     unless params[:cgi_escaped_url].nil?
       url = CGI.unescape(params[:cgi_escaped_url])
       if URI.parse(url).host == IDOBATA_HOST
         Idobata.hook_url = url
       end
     end
+
     Thread.new do
       if ENV['BRANCH'] == 'all' || data["ref"].match(/#{ENV['BRANCH']}/)
         message = Slim::Template.new("templates/push.slim").render(self, data: data)
